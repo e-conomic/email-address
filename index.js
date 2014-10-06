9f963b1dca143f3d30b0b2a8ff5deb6ea93f7c1b
Martin Gausby <martin@gausby.dk>
2014-10-06 11:47:14 +0200

    fixes #1 handle accents and umlauts

--- index.js
+++ index.js
@@ -2,11 +2,23 @@
 
 // Email validation that does handle CJK, Cyrillic, Devangari, Hiragana,
 // Arabic and require at least 2 letters in top-level domain.
+var validChars = function(extra, quantifier) {
+	var accents = 'ÁáÀàÂâÆæÄäÇçÉéÈèÊêËëÍíÎîÏïÑñÓóÔôŒœÖöÚúÙùÛûÜüŸÿ';
+	var unicodes = '\\u4e00-\\u9eff\\u0400-\\u04ff\\u0900-\\u097f\\u3040-\\u309f\\u0600-\\u06ff';
+	extra = typeof extra === 'string' ? extra : '';
+	quantifier = (['*', '+', '?'].indexOf(quantifier) > -1 ? quantifier : '');
 
-// Derived from:
-// [a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])
-// this should handle 99.99% of all email addresses in use today.
-var emailPattern = /^[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff!#$%&'*+/=?^_`{|}~-]+(?:\.[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff!#$%&'*+/=?^_`{|}~-]+)*@(?:[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff](?:[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff-]*[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff])?\.)+[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff](?:[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff-]*[A-Za-z0-9\u4e00-\u9eff\u0400-\u04ff\u0900-\u097f\u3040-\u309f\u0600-\u06ff])$/i;
+	return [
+		'[','A-Za-z0-9', accents, unicodes, extra,']',
+		quantifier
+	].join('');
+};
+
+var local = validChars("!#$%&'*+/=?^_`{|}~-", '+')+"(?:\\."+validChars("!#$%&'*+/=?^_`{|}~-", '+')+")*";
+var domain = "(?:"+validChars()+"(?:"+validChars('-', '*')+validChars()+")?\\.)+";
+var topLevelDomain = validChars()+"(?:"+validChars('-', '*')+validChars()+")";
+
+var emailPattern = new RegExp('^' + local + '@' + domain + topLevelDomain + '$', 'i');
 
 var emailPatternSource = emailPattern.source.slice(1, -1);
 var multipleEmailPattern = new RegExp('^\\s*(?:' + emailPatternSource + '\\s*,\\s*)*' + emailPatternSource + '$');
